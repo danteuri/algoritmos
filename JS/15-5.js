375b7ff8487c9b28d50e022330538546f54e8b11
danteuri <santiagomaytahuanca@gmail.com>
2023-05-15 17:38:16 -0300

    A
    A

--- JS/15-5.js
+++ JS/15-5.js
@@ -1,9 +1,15 @@
+let nume = 556;
 function ejemplo(params) {
     let inp = document.querySelector("#enter");
     let p = document.querySelector("#salida");
+    let divImg = document.querySelector("#img");
+    divImg.innerHTML = '<img src="../IV/descarga.jpg" alt="error"/>'
     let valor = inp.value;
-    p.innerTex =valor;
+    p.innerText = valor;
     console.log(p.value);
     console.log(valor);
     console.dir(inp);
+    console.log(nume);
+    let r = nume;
+    console.log(r);
 }
