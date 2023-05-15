d7b99f8e35d314141aadc1cb1afc2bbefe78b8f5
danteuri <santiagomaytahuanca@gmail.com>
2023-05-15 16:33:53 -0300

    u
    u

--- JS/15-5.js
+++ JS/15-5.js
@@ -1,6 +1,9 @@
 function ejemplo(params) {
     let inp = document.querySelector("#enter");
+    let p = document.querySelector("#salida");
     let valor = inp.value;
+    p.innerTex =valor;
+    console.log(p.value);
     console.log(valor);
-    console.log(inp);
+    console.dir(inp);
 }
