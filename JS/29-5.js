616ffd155ed0e9de9e460ea781834dbfe7eaea4f
danteuri <132934430+danteuri@users.noreply.github.com>
2023-05-29 17:11:08 -0300

    proposito
    A

--- JS/29-5.js
+++ JS/29-5.js
@@ -8,10 +8,28 @@ function meses123() {
 function elpepe(){
 var valores = [true, 5, false, "hola", "adios", 2];
 var aux = [];
+var posicion = 0;
+var valor = "";
+
 for (let i = 0; i < valores.length; i++){
     if(typeof(valores[i]) == "string"){
         aux.push(valores[i]);
     }
 }
-console.log(valores);
+
+for(let  i = 0; i < aux.length -1; i++) {
+    if (aux[i].length > aux[i+1].length && i+1 < aux.length) {
+        posicion = i;
+        valor = aux[i + 1];
+    }
+}
+
+console.log("el mayor es "+valor);
+
+if (valores[3].length > valores[4].length) {
+    console.log("hola es mayor");
+}
+else{
+    console.log("adios es mayor");
+}
 }
