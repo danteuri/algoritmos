cec50946a0f056ee3ec9387040d767452bf6fb4a
danteuri <132934430+danteuri@users.noreply.github.com>
2023-05-29 16:33:12 -0300

    t
    t

--- JS/29-5.js
+++ JS/29-5.js
@@ -1,4 +1,17 @@
+
+//Ejercicio 1
 function meses123() {
     let meses123 = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
     console.log(meses123);
+}
+//Ejercicio 2
+function elpepe(){
+var valores = [true, 5, false, "hola", "adios", 2];
+var aux = [];
+for (let i = 0; i < valores.length; i++){
+    if(typeof(valores[i]) == "string"){
+        aux.push(valores[i]);
+    }
+}
+console.log(valores);
 }
